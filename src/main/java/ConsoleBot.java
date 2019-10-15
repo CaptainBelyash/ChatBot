83619a680cb7dedbf4099943ac0bdaa7ceeaa57c
CaptainBelyash <50026308+CaptainBelyash@users.noreply.github.com>
2019-10-15 19:23:37 +0500

    Revert "God pls help me"
    This reverts commit 253096ee46a70b91ecb4dd54efb0ff57a1256cfc.

--- src/main/java/ConsoleBot.java
+++ src/main/java/ConsoleBot.java
@@ -6,6 +6,7 @@ import java.util.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.function.Function;
 
 public class ConsoleBot {
     public static HashMap<String, Command> commandsList = new HashMap<>();
