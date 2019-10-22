6fc94cb617eaa130b9bb6dfcfd56bd99e897536a
CaptainBelyash <50026308+CaptainBelyash@users.noreply.github.com>
2019-10-22 21:37:41 +0500

    delete proxy settings
    Удалил ненужную шляпу

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,10 +6,6 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.getProperties().put("proxySet", "true");
-        System.getProperties().put("socksProxyHost", "127.0.0.1");
-        System.getProperties().put("socksProxyPort", "9150");
-
         ApiContextInitializer.init();
         TelegramBotsApi bot = new TelegramBotsApi();
 
