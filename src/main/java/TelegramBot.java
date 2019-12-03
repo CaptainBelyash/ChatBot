0a5c78de8d28f573a9c9975c2db38648826c9d67
CaptainBelyash <50026308+CaptainBelyash@users.noreply.github.com>
2019-12-03 19:51:32 +0500

    test notify

--- src/main/java/TelegramBot.java
+++ src/main/java/TelegramBot.java
@@ -4,9 +4,15 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 public class TelegramBot extends TelegramLongPollingBot {
     private BotLogic bot = new BotLogic();
+    
+    public TelegramBot(){
+        var thread = new NotifyThread(this);
+        thread.start();
+    }
 
     public synchronized void sendMsg(String chatId, String input) throws TelegramApiException {
         SendMessage sendMess = new SendMessage();
@@ -18,6 +24,33 @@ public class TelegramBot extends TelegramLongPollingBot {
 
         }
         execute(sendMess);
+    }
+
+
+    private static class NotifyThread extends Thread{
+        private HashMap<String, Pet> pets;
+        private TelegramBot tgBot;
+        NotifyThread(TelegramBot tgBot){
+            this.tgBot = tgBot;
+            pets = this.tgBot.bot.getPets();
+        }
+
+        @Override
+        public void run(){
+            for (String playerID:
+                 pets.keySet()) {
+                try {
+                    tgBot.sendMsg(playerID, pets.get(playerID).getCharacteristics());
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+            }
+            try {
+                Thread.sleep(20000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
