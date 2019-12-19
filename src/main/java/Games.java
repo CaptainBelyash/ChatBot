9b4a75e73a131b5a1f9aa801a021189adcd60aad
Irina-zaez <Irina-zaez@yandex.ru>
2019-12-19 15:16:41 +0500

    Fix

--- src/main/java/Games.java
+++ src/main/java/Games.java
@@ -9,11 +9,6 @@ public class Games {
     public Games() {
         gamesAssortment = new HashMap<>();
         gamesAssortment.put("GuessNumber", "Питомец загадывает число от 0 до 100. Вы должны угадать число.");
-        gamesAssortment.put("BullsAndCows", "Питомец загадывает пятибуквенное слово без повторяющихся букв. " +
-                "Вы должны называть ему пятибуквенные слова без повторяющихся букв. " +
-                "После чего питомец называет количество коров и быков." +
-                "Коровы - количество букв в вашем слове, которые содержатся в загаданном. " +
-                "Быки - количество букв в вашем слове, которые стоят на тех же позициях, что и в загаданном.");
     }
 
     public void play(String gameName) {
@@ -21,19 +16,13 @@ public class Games {
         if (currentGame.equals("GuessNumber")) {
             this.guessNumber = new GuessTheNumberGame();
         }
-        if (currentGame.equals("BullsAndCows")) {
-            this.bullsAndCows = new BullsAndCows();
-        }
     }
 
     public String doAction(String[] args) {
         if (currentGame.equals("GuessNumber")) {
             return this.guessNumber.makeGuess(Integer.parseInt(args[0]));
         }
-        if (currentGame.equals("BullsAndCows")) {
-            return this.bullsAndCows.makeGuess(args[0]);
-        }
-        return "Произошла ошибка";
+        return "Такой игры нет";
     }
 
     public static String getGamesAssortment() {
@@ -49,18 +38,12 @@ public class Games {
         if (currentGame.equals("GuessNumber")) {
             return guessNumber.getWin();
         }
-        if (currentGame.equals("BullsAndCows")) {
-            return bullsAndCows.getWin();
-        }
         return false;
     }
 
     public int getMoney() {
         if (currentGame.equals("GuessNumber")) {
             return 50 - guessNumber.getTries();
-        }
-        if (currentGame.equals("BullsAndCows")) {
-            return 100 - bullsAndCows.getTries();
         }
         return 0;
     }
